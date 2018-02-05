3199b4248f522233b4c281bc484ecc4ffd2065c6
郑尧元 <zhengyaoyuan@zhengyaoyuandeMacBook-Pro.local>
2018-02-05 09:38:22 +0800

    upload deleteObj

---
+++ ZYYMianshiDemo/ZYYMianshiDemo/ZYYDeleteObj.h
@@ -0,0 +1,13 @@
+//
+//  ZYYDeleteObj.h
+//  ZYYMianshiDemo
+//
+//  Created by 郑尧元 on 2018/2/5.
+//  Copyright © 2018年 郑尧元. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZYYDeleteObj : NSObject
+
+@end
---
+++ ZYYMianshiDemo/ZYYMianshiDemo/ZYYDeleteObj.m
@@ -0,0 +1,13 @@
+//
+//  ZYYDeleteObj.m
+//  ZYYMianshiDemo
+//
+//  Created by 郑尧元 on 2018/2/5.
+//  Copyright © 2018年 郑尧元. All rights reserved.
+//
+
+#import "ZYYDeleteObj.h"
+
+@implementation ZYYDeleteObj
+
+@end
